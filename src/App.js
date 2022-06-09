2252ebef7cb36c72da5a8a8e364186a44f682b7a
Arnoldas R <arokon.arnoldas.reklys@gmail.com>
2022-06-09 12:39:07 +0300

    First base React commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+import greek from  './greek.svg';
+import romanstatue  from './roman-statue.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={greek} className="App-logo" alt="logo"/>
+        <img src={romanstatue} className='App-roman' alt='roman'/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
